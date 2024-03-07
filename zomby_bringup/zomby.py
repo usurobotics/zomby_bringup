d04734ce200287d6fde2345712c1d8091e21d003
Joshua Taggart <jftaggart02@gmail.com>
2024-03-06 20:37:07 -0700

    added zomby lib code

---
+++ zomby_bringup/zomby.py
@@ -0,0 +1,77 @@
+import serial
+
+DEBUG = 1
+
+class Zomby:
+    def wait_for_arduino(self):
+
+        # waits to receive this character
+        arduino_ready_signal = 'R'
+        
+        waiting = True
+        
+        print("Waiting for Arduino...")
+
+        while waiting:
+            if self.__serial_port.in_waiting > 0:
+                char = self.__serial_port.read(1).decode('utf-8')
+                if char == arduino_ready_signal:
+                    print("Arduino is ready.")
+                    waiting = False
+
+
+    def __init__(self, port, baud_rate):
+        # Initialize serial port
+        self.__serial_port = serial.Serial(port, baud_rate)
+
+        self.__ser_ID_right = b"r"
+        self.__ser_ID_left = b"l"
+
+
+        # Wait for arduino to be ready
+        self.wait_for_arduino()
+
+    def setSpeed(self, right_speed, left_speed):
+        if right_speed > 128:
+            right_speed_sat = 128
+        elif right_speed < 0:
+            right_speed_sat = 0
+        else:
+            right_speed_sat = right_speed
+
+        if left_speed > 128:
+            left_speed_sat = 128
+        elif left_speed < 0:
+            left_speed_sat = 0
+        else:
+            left_speed_sat = left_speed
+
+        if DEBUG:
+            print("send speed ")
+        # send right motor ID to arduino
+        self.__serial_port.write(self.__ser_ID_right)
+
+        # DEBUG
+        if DEBUG:
+            print(self.__ser_ID_right)
+
+        # send right motor speed to arduino
+        self.__serial_port.write(right_speed_sat.to_bytes(length=1, byteorder='big'))
+
+        # DEBUG
+        if DEBUG:
+            print(right_speed_sat)
+
+        # send left motor ID to arduino
+        self.__serial_port.write(self.__ser_ID_left)
+
+        # DEBUG
+        if DEBUG:
+            print(self.__ser_ID_left)
+
+        # send left motor speed to arduino
+        self.__serial_port.write(left_speed_sat.to_bytes(length=1, byteorder='big'))
+
+        # DEBUG
+        if DEBUG:
+            print(left_speed_sat)
---
+++ zomby_bringup/zomby_handshake_test.py
@@ -0,0 +1,48 @@
+import zomby
+import time as pytime
+
+if __name__ == "__main__":
+    robot = zomby.Zomby(port="COM9", baud_rate=115200)
+
+    max_speed: int = 90
+
+    min_speed: int = 40
+
+    speed: int = 64 # start at stopped!
+
+    step: int = 1
+
+    increment: bool = True
+
+    count: int = 0
+
+    num_cycles: int = 4
+
+    delay: float = 0.05
+
+    run: bool = True
+
+
+    while run:
+        robot.wait_for_arduino()
+        robot.setSpeed(
+            left_speed=speed, 
+            right_speed=speed
+        )
+
+        if speed == max_speed:
+            increment = False
+
+        if not increment and speed == min_speed:
+            increment = True
+            count += 1
+
+        if speed == 64 and count == num_cycles:
+            run = False
+
+        if increment:
+            speed += step
+        else:
+            speed -= step
+
+        pytime.sleep(delay)
